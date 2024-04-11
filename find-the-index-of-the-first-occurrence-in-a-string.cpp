d8d86988763ea93460ef928855bba6b47fcbee90
Sofus Lund <sofuslund.dev@gmail.com>
2024-04-11 20:14:31 +0200

    Update find-the-index-of-the-first-occurrence.cpp to faster version (KMP algorithm) and save the old one

--- find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,12 +1,43 @@
-#include <vector>
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        for(int i = 0; i+needle.size()-1< haystack.size(); i++) {
-            if (haystack.substr(i, needle.size()) == needle) {
-                return i;
+        // Compute KMP table
+        int matchTable[needle.size()];
+        memset(matchTable, 0, sizeof(matchTable));
+        int lps = 0; // Longest prefix that is also a suffix for i length
+        for (int i = 1; i < needle.size(); i++) {
+            if(needle[lps]==needle[i]) { // Lps continues and increases by one
+                lps++;
+            } else { // Lps is smaller
+                while(lps > 0) {
+                    bool b = true;
+                    for (int j = 0; j < lps; j++) {
+                        if (needle[j] != needle[i-lps+1+j])
+                            b = false;
+                    }
+                    if(b)
+                        break;
+                    lps--;
+                }
             }
+            matchTable[i] = lps;
         }
+        int count = 0;
+        for (int i = 0; i < haystack.size(); i++) {
+            comparison:
+            if(haystack[i] == needle[count]) {
+                count++;
+            } else {
+                if (count>0) {
+                    count = matchTable[count-1];
+                    goto comparison;
+                }
+            }
+            if(count == needle.size())
+                return i-count+1; // Where match starts
+            
+        }
+
         return -1;
     }
 };
