2f75531ccb3e8416d419640172f010f64a831731
Sofus Lund <sofuslund.dev@gmail.com>
2024-04-15 17:49:56 +0200

    Add next-permutation.cpp

---
+++ next-permutation.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i;
+        for(i = nums.size()-1; i>=1 && nums[i] <= nums[i-1]; i--) {}
+        // I is now the first number bigger than its previous element (sorted)
+        
+        if(i!=0) {
+            int next = nums.size()-1;
+            // Set next to smallest number bigger than i-1 
+            while(next >= i && nums[next] <= nums[i-1]) {
+                next--;
+            }
+            iter_swap(nums.begin()+i-1, nums.begin() + next);
+        }
+        sort(nums.begin()+i, nums.end());
+    }
+};
