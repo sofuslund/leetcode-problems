e87b0c06db1087a6a383831b16cf05c328c2ca66
Sofus Lund <sofuslund.dev@gmail.com>
2025-06-11 20:26:18 +0200

    Update n-queens

--- n-queens/n-queens.py
+++ n-queens/n-queens.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 def solveNQueens(n):
+    tree = [defaultdict(int) for _ in range(n)]
     leaves = 0 # How many leaf leaves were visited in the imaginary search tree
     solutions = []
     # Recursive function to explore solutions
@@ -11,23 +13,24 @@ def solveNQueens(n):
             leaves += 1
             solutions.append([''.join('.' if elm == 0 else 'Q' for elm in row) for row in brd])
             return
-        dead_end = True
+        free_places = 0
         for col in free_cols:
             rdia = curr_row + col
             ldia = curr_row + n-1-col
             if rdia not in free_rdias or ldia not in free_ldias:
                 continue
-            dead_end = False
+            free_places += 1
+            # tree[0]
             new_brd = [row[:] for row in brd]
             new_brd[curr_row][col] = 1
             nqueens(new_brd, curr_row + 1, free_cols - {col}, free_rdias - {rdia}, free_ldias - {ldia})
-        if dead_end:
+        if free_places == 0: # dead end
             leaves +=1
 
     nqueens([[0]*n]*n, 0, set(range(n)),set(range(2*n-1)),set(range(2*n-1)))
-    # if n == 4:
-    #     print(nqueens([[0, 1, 0, 0], [0, 0, 0, 1], [1, 0, 0, 0], [0, 0, 0, 0]], 3, {2, 3}, ))
-    print(f"N-Queens n={n}: Explored {leaves} possibilit{'ies' if leaves!=1 else 'y'} out of which {len(solutions)} were solutions.")
+    print(f"N-Queens n={n}: Explored {leaves} possibilit{'ies' if leaves!=1 else 'y'} out of which {len(solutions)} were solutions.", end=' ')
+    if input("Show tree? (y/n): ").lower() == 'y':
+        pass
     return solutions
 
 total_solutions = [solveNQueens(i) for i in range(1, int(input("Enter the maximum n for N-Queens (default 9): ") or 9)+1)]
